45ea0ce60e5d8fa7cacacd9734ef5e423d47fee2
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-04 01:20:38 -0300

    refactor: exercise correction

--- dml/consultas_simples.sql
+++ dml/consultas_simples.sql
@@ -5,51 +5,51 @@ SELECT nome, genero, profissao FROM clientes
 ORDER BY nome DESC;
 
 -- 2. Os clientes que tenham a letra “R” no nome
-SELECT * FROM clientes
+SELECT nome FROM clientes
 WHERE nome LIKE '%r%';
 
 -- 3. Os clientes que o nome inicia com a letra “C”
-SELECT * FROM clientes
+SELECT nome FROM clientes
 WHERE nome LIKE 'C%';
 
 -- 4. Os clientes que o nome termina com a letra “A”
-SELECT * FROM clientes
+SELECT nome FROM clientes
 WHERE nome LIKE '%a';
 
 -- 5. Os clientes que moram no bairro “Centro”
-SELECT * FROM clientes
-WHERE bairro = 'Centro';
+SELECT nome, bairro FROM clientes
+WHERE bairro = 'Centro' OR bairro = 'Cto.' OR bairro = 'Ctr.';
 
 -- 6. Os clientes que moram em complementos que iniciam com a letra “A”
-SELECT * FROM clientes
+SELECT nome, complemento FROM clientes
 WHERE complemento LIKE 'A%';
 
 -- 7. Somente os clientes do sexo feminino
-SELECT * FROM clientes
+SELECT nome, genero FROM clientes
 WHERE genero = 'F';
 
 -- 8. Os clientes que não informaram o CPF
-SELECT * FROM clientes
+SELECT nome, cpf FROM clientes
 WHERE cpf IS NULL;
 
 -- 9. O nome e a profissão dos clientes, ordenado em ordem crescente pelo nome da profissão
 SELECT nome, profissao FROM clientes
-ORDER BY profissao ASC;
+ORDER BY profissao;
 
 -- 10. Os clientes de nacionalidade “Brasileira”
-SELECT * FROM clientes
-WHERE nacionalidade = 'Brasileira';
+SELECT nome, nacionalidade FROM clientes
+WHERE nacionalidade LIKE 'Brasil%';
 
 -- 11. Os clientes que informaram o número da residência
-SELECT * FROM clientes
+SELECT nome, numero FROM clientes
 WHERE numero IS NOT NULL;
 
 -- 12. Os clientes que moram em Santa Catarina
-SELECT * FROM clientes
+SELECT nome, uf FROM clientes
 WHERE uf = 'SC';
 
 -- 13. Os clientes que nasceram entre 01/01/2000 e 01/01/2002
-SELECT * FROM clientes
+SELECT nome, data_nascimento FROM clientes
 WHERE data_nascimento BETWEEN '2000-01-01' AND '2002-01-01';
 
 -- 14. O nome do cliente e o logradouro, número, complemento, bairro, município e UF concatenado de todos os clientes
