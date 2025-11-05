4cfe798fc52746137252be9791a83a0dc2082126
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-05 13:07:27 -0300

    feat: update clientes table to use foreign key for profissao

--- ddl/clientes.sql
+++ ddl/clientes.sql
@@ -62,3 +62,40 @@ VALUES (14,'Jeferson',null,null,'1983-07-01','M',null,'Brasileiro',null,null, nu
 
 INSERT INTO clientes(id, nome, cpf, rg, data_nascimento, genero, profissao, nacionalidade, logradouro, numero, complemento, bairro, municipio, UF)
 VALUES (15,'Jessica',null,null,null,'F','Estudante', null, null, null, null, null, 'União da Vitória','PR');
+
+ALTER TABLE clientes
+RENAME COLUMN profissao TO id_profissao;
+
+ALTER TABLE clientes
+DROP id_profissao;
+
+ALTER TABLE clientes
+ADD id_profissao INTEGER;
+
+ALTER TABLE clientes
+ADD CONSTRAINT fk_cln_idprofissao FOREIGN KEY (id_profissao) REFERENCES profissoes (id);
+
+UPDATE clientes
+SET
+	id_profissao = 1
+WHERE id IN (1, 9, 10, 12, 15, 17);
+
+UPDATE clientes
+SET
+	id_profissao = 2
+WHERE id = 2;
+
+UPDATE clientes
+SET
+	id_profissao = 3
+WHERE id = 3;
+
+UPDATE clientes
+SET
+	id_profissao = 4
+WHERE id IN (4, 5);
+
+UPDATE clientes
+SET
+	id_profissao = 5
+WHERE id IN (6, 7, 8, 13);
