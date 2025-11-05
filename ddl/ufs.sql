424721aed1a74085c23602b4e4989e6191834880
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-05 15:14:39 -0300

    feat: create federal units table with initial data and constraints

---
+++ ddl/ufs.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS ufs (
+	id INTEGER NOT NULL,
+	nome VARCHAR(30) NOT NULL,
+	sigla CHAR(2) NOT NULL,
+
+	CONSTRAINT pk_ufd_id PRIMARY KEY (id),
+	CONSTRAINT un_ufd_nome UNIQUE (nome),
+	CONSTRAINT un_ufd_sigla UNIQUE (sigla)
+);
+
+INSERT INTO ufs (id, nome, sigla)
+VALUES (1, 'São Paulo', 'SP');
+
+INSERT INTO ufs (id, nome, sigla)
+VALUES (2, 'Rio Grande do Sul', 'RS');
+
+INSERT INTO ufs (id, nome, sigla)
+VALUES (3, 'Santa Catarina', 'SC');
+
+INSERT INTO ufs (id, nome, sigla)
+VALUES (4, 'Paraná', 'PR');
+
+SELECT * FROM ufs;
