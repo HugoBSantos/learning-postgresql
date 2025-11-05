ef483ca4bcc60d7dcaf662059da07a8c71c33b64
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-05 13:40:44 -0300

    feat: implement foreign key for nacionalidade in clientes table

--- ddl/chaves_estrangeiras.sql
+++ ddl/chaves_estrangeiras.sql
@@ -15,26 +15,51 @@ ADD CONSTRAINT
 	REFERENCES profissoes (id);
 
 UPDATE clientes
-SET
-	id_profissao = 1
-WHERE id IN (1, 9, 10, 12, 15, 17);
+SET id_profissao = 1
+WHERE id IN (1,9,10,12,15,17);
 
 UPDATE clientes
-SET
-	id_profissao = 2
+SET id_profissao = 2
 WHERE id = 2;
 
 UPDATE clientes
-SET
-	id_profissao = 3
+SET id_profissao = 3
 WHERE id = 3;
 
 UPDATE clientes
-SET
-	id_profissao = 4
+SET id_profissao = 4
 WHERE id IN (4, 5);
 
 UPDATE clientes
-SET
-	id_profissao = 5
-WHERE id IN (6, 7, 8, 13);
+SET id_profissao = 5
+WHERE id IN (6,7,8,13);
+
+
+
+-- Nacionalidade
+ALTER TABLE clientes
+DROP nacionalidade;
+
+ALTER TABLE clientes
+ADD id_nacionalidade INTEGER;
+
+ALTER TABLE clientes
+ADD CONSTRAINT
+	fk_cln_idnacionalidade FOREIGN KEY (id_nacionalidade)
+	REFERENCES nacionalidades (id);
+
+UPDATE clientes
+SET id_nacionalidade = 1
+WHERE id IN (1,2,3,4,6,10,11,14);
+
+UPDATE clientes
+SET id_nacionalidade = 2
+WHERE id IN (5,7);
+
+UPDATE clientes
+SET id_nacionalidade = 3
+WHERE id = 8;
+
+UPDATE clientes
+SET id_nacionalidade = 4
+WHERE id IN (9,13);
