1d425f28a939947c51f52f7a0ee59700f030bef3
HugoSantos14 <hugosantos14000@gmail.com>
2025-11-11 23:08:58 -0300

    feat: add default values for columns in pedidos and produto tables

---
+++ ddl/valores_default.sql
@@ -0,0 +1,25 @@
+ALTER TABLE pedidos
+ALTER COLUMN data_pedido
+SET DEFAULT CURRENT_DATE;
+
+ALTER TABLE pedidos
+ALTER COLUMN valor
+SET DEFAULT 0;
+
+-- Exercícios valores default
+-- 1. Adicione valores default na tabela de produtos do pedido
+-- a. Quantidade com o valor 1
+ALTER TABLE pedido_produto
+ALTER COLUMN quantidade
+SET DEFAULT 1;
+
+-- b. Valor unitário com o valor 0
+ALTER TABLE pedido_produto
+ALTER COLUMN valor_unitario
+SET DEFAULT 0;
+
+-- 2. Adicione valor default na tabela de produtos
+-- a. Valor com o valor 0
+ALTER TABLE produtos
+ALTER COLUMN valor
+SET DEFAULT 0;
